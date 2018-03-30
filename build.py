472192978f0664e98513ab848e323f34e973466b
Stefano Bragaglia <stefano.bragaglia@gmail.com>
2018-03-30 23:23:31 +0100

    Initial commit

---
+++ build.py
@@ -0,0 +1,27 @@
+from pybuilder.core import use_plugin, init
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.install_dependencies")
+use_plugin("python.flake8")
+use_plugin("python.coverage")
+use_plugin("python.distutils")
+use_plugin("python.pycharm")
+
+name = "grapes"
+version = "0.0.1"
+default_task = ["clean", "analyze", "publish"]
+
+
+@init
+def set_properties(project):
+    project.set_property("flake8_break_build", True)  # default is False
+    project.set_property("flake8_verbose_output", True)  # default is False
+    project.set_property("coverage_break_build", True)  # default is False
+    project.set_property("flake8_radon_max", 10)  # default is None
+    project.set_property_if_unset("flake8_max_complexity", 10)  # default is None
+    # Complexity: <= 10 is easy, <= 20 is complex, <= 50 great difficulty, > 50 unmaintainable
+
+    project.set_property("dir_source_unittest_python", "src/test/python")
+
+    project.depends_on_requirements("requirements.txt")
