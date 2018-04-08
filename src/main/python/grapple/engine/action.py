93e228c3f7a3a4a513e4b1b8306dd89fda33da18
Stefano Bragaglia <stefano.bragaglia@gmail.com>
2018-04-08 11:49:12 +0100

    Complete descriptors for patterns and return actions; implement ReturnAction

--- src/main/python/grapple/engine/action.py
+++ src/main/python/grapple/engine/action.py
@@ -3,14 +3,42 @@ from grapple.engine.activation import Params
 Record = Params
 
 
+class ActionError(Exception):
+    pass
+
+
 class Action(object):
-
-    def __init__(self, pattern: 'Pattern') -> None:
-        self._pattern = pattern
-
-    @property
-    def pattern(self) -> 'Pattern':
-        return self._pattern
 
     def apply(self, params: Params, graph: 'Graph') -> Record:
         raise NotImplementedError('To be implemented')
+
+
+class ReturnAction(Action):
+
+    def __init__(self, descriptor: 'ReturnDescriptor'):
+        self._descriptor = descriptor
+
+    @property
+    def descriptor(self) -> 'ReturnDescriptor':
+        return self._descriptor
+
+    def apply(self, params: Params, graph: 'Graph') -> Record:
+        record = {}
+        for descriptor in self._descriptor.records:
+            if descriptor.variable not in params:
+                raise ValueError("Variable '%s' not defined" % descriptor.variable)
+
+            if descriptor.name:
+                key = descriptor.name
+            elif descriptor.property:
+                key = '.'.join([descriptor.variable, descriptor.name])
+            else:
+                key = descriptor.variable
+
+            value = params[descriptor.variable]
+            if descriptor.property:
+                value = value.get_property(descriptor.property)
+
+            record[key] = value
+
+        return record
--- src/main/python/grapple/engine/descriptors.py
+++ src/main/python/grapple/engine/descriptors.py
@@ -1,9 +1,39 @@
 from valid_model import Object
-from valid_model.descriptors import String, List, Dict
+from valid_model.descriptors import String, List, Dict, EmbeddedObject, Bool, Integer
 
 
-class NodeDescriptor(Object):
+class EntityDescriptor(Object):
     variable = String()
     labels = List(value=String(nullable=False))
-    properties = Dict(key=String(nullable=False), value=)
-    pass
+    properties = Dict(key=String(nullable=False))
+
+
+class PathDescriptor(Object):
+    variable = String()
+    entities = List(value=EmbeddedObject(EntityDescriptor))
+
+
+class PatternDescriptor(Object):
+    paths = List(value=EmbeddedObject(PathDescriptor))
+
+
+class RecordDescriptor(Object):
+    variable = String(nullable=False)
+    property = String()
+    name = String()
+
+
+class OrderDescriptor(Object):
+    variable = String()
+    property = String()
+    name = String()
+    asc = Bool(default=True)
+
+
+class ReturnDescriptor(Object):
+    records = List(value=EmbeddedObject(RecordDescriptor(nullable=False)))
+    ordering = List(value=EmbeddedObject(OrderDescriptor(nullable=False)))
+    limit = Integer(default=-1)
+
+
+if __name__ == '__main__':
