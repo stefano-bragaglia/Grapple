c89d0597434d4f00a17465d4b949073140e94d23
Stefano Bragaglia <stefano.bragaglia@gmail.com>
2018-04-23 23:34:48 +0100

    Complete refactoring of base grammar (WHERE, CREATE, DELETE, SET still missing)

--- src/main/python/grapple/parsing/grammar.py
+++ src/main/python/grapple/parsing/grammar.py
@@ -1,10 +1,53 @@
-from arpeggio import EOF, Optional, ParserPython, RegExMatch, ZeroOrMore
+from arpeggio import EOF, Optional, ParserPython, RegExMatch, ZeroOrMore, OneOrMore
 
 
-def base(): Optional(rules), EOF
+def comment(): return [RegExMatch(r"/\*.*\*/"), RegExMatch(r"//.*")]
 
-def rules(): return rule, ZeroOrMore(rule)
-def rule(): return part_return, ";"
+def key_as(): return RegExMatch(r"AS", ignore_case=True)
+def key_false(): return RegExMatch(r"FALSE", ignore_case=True)
+def key_id(): return RegExMatch(r"ID", ignore_case=True)
+def key_labels(): return RegExMatch(r"LABELS", ignore_case=True)
+def key_match(): return RegExMatch(r"MATCH", ignore_case=True)
+def key_null(): return RegExMatch(r"NULL", ignore_case=True)
+def key_return(): return RegExMatch(r"RETURN", ignore_case=True)
+def key_rule(): return RegExMatch(r"RULE", ignore_case=True)
+def key_salience(): return RegExMatch(r"SALIENCE", ignore_case=True)
+def key_true(): return RegExMatch(r"TRUE", ignore_case=True)
+def key_types(): return RegExMatch(r"TYPES", ignore_case=True)
+
+def json_object(): return "{", Optional(json_members), "}"
+def json_members(): return json_member, ZeroOrMore(",", json_member)
+def json_member(): return json_string, ":", json_value
+def json_value(): return [json_string, json_number, json_object, json_array, key_true, key_false, key_null]
+def json_string(): return '"', RegExMatch('[^"]*'), '"'
+def json_number(): return RegExMatch('-?\d+((\.\d*)?((e|E)(\+|-)?\d+)?)?')
+def json_array(): return "[", Optional(json_elements), "]"
+def json_elements(): return json_value, ZeroOrMore(",", json_value)
+
+def flag(): return RegExMatch(r":[a-zA-Z]\w*")
+def identifier(): return RegExMatch(r"[a-zA-Z]\w*")
+def variable(): return RegExMatch(r"\$[a-zA-Z]\w*")
+
+
+def resource(): return ZeroOrMore(statement), EOF
+
+def statement(): return part_rule, part_match, part_return, ";"
+
+def part_rule(): return description, Optional(salience)
+def description(): return key_rule, Optional(json_string)
+def salience(): return key_salience, json_number
+
+def part_match(): return OneOrMore(key_match, path)
+def path(): return node, ZeroOrMore(relation, node)
+def relation(): return [relation_rwd, relation_fwd, relation_any]
+def relation_rwd(): return "<-", Optional(relation_def), "-"
+def relation_fwd(): return "-", Optional(relation_def), "->"
+def relation_any(): return "-", Optional(relation_def), "-"
+def relation_def(): return "[", Optional(assignment), Optional(flags), Optional(attributes), "]"
+def node(): return "(", Optional(assignment), Optional(flags), Optional(attributes), ")"
+def assignment(): return variable
+def flags(): return flag, ZeroOrMore(flag)
+def attributes(): return json_object
 
 def part_return(): return key_return, selectors
 def selectors(): return selector, ZeroOrMore(",", selector)
@@ -17,25 +60,4 @@ def ident(): return key_id, "(", variable, ")"
 def accessor(): return variable, Optional(".", identifier)
 def synonym(): return Optional(key_as, identifier)
 
-def identifier(): return RegExMatch(r"[a-zA-Z]\w*")
-def variable(): return RegExMatch(r"\$[a-zA-Z]\w*")
 
-def json_object(): return "{", Optional(json_members), "}"
-def json_members(): return json_member, ZeroOrMore(",", json_member)
-def json_member(): return json_string, ":", json_value
-def json_value(): return [json_string, json_number, json_object, json_array, key_true, key_false, key_null]
-def json_string(): return '"', RegExMatch('[^"]*'), '"'
-def json_number(): return RegExMatch('-?\d+((\.\d*)?((e|E)(\+|-)?\d+)?)?')
-def json_array(): return "[", Optional(json_elements), "]"
-def json_elements(): return json_value, ZeroOrMore(",", json_value)
-
-def key_as(): return RegExMatch(r"AS", ignore_case=True)
-def key_false(): return RegExMatch(r"FALSE", ignore_case=True)
-def key_id(): return RegExMatch(r"ID", ignore_case=True)
-def key_labels(): return RegExMatch(r"LABELS", ignore_case=True)
-def key_null(): return RegExMatch(r"NULL", ignore_case=True)
-def key_return(): return RegExMatch(r"RETURN", ignore_case=True)
-def key_true(): return RegExMatch(r"TRUE", ignore_case=True)
-def key_types(): return RegExMatch(r"TYPES", ignore_case=True)
-
-def comment(): return [RegExMatch(r"/\*.*\*/"), RegExMatch(r"//.*")]
--- src/main/python/grapple/parsing/visitors.py
+++ src/main/python/grapple/parsing/visitors.py
@@ -2,11 +2,81 @@ import json
 
 from arpeggio import PTNodeVisitor, visit_parse_tree
 
+from grapple.engine.descriptors import Direction
 from grapple.parsing.grammar import *
 
 
 # noinspection PyMethodMayBeStatic
 class GrammarVisitor(PTNodeVisitor):
+
+    def visit_resource(self, node, children) -> object:
+        return {'value': [child['value'] for child in children]}
+
+    def visit_statement(self, node, children) -> object:
+        result = {}
+        for child in children:
+            result.update(child['value'])
+        return {'value': result}
+
+    def visit_part_rule(self, node, children) -> object:
+        result = {'description': None, 'salience': 0}
+        for child in children:
+            result.update(child['value'])
+        return {'value': result}
+
+    def visit_description(self, node, children) -> object:
+        return {'value': {'description': children[1]['value'] if len(children) > 1 else None}}
+
+    def visit_salience(self, node, children) -> object:
+        return {'value': {'salience': children[1]['value']}}
+
+    def visit_part_match(self, node, children) -> object:
+        return {'value': {'match': [child['value'] for child in children if child['value'] != 'MATCH']}}
+
+    def visit_path(self, node, children) -> object:
+        return {'value': [child['value'] for child in children]}
+
+    def visit_relation(self, node, children) -> object:
+        return {'value': children[0]['value']}
+
+    def visit_relation_rwd(self, node, children) -> object:
+        result = {'type': 'relation', 'direction': 1, 'variable': None, 'flags': None, 'attributes': None}
+        if children:
+            for child in children[0]['value']:
+                result.update(child)
+        return {'value': result}
+
+    def visit_relation_fwd(self, node, children) -> object:
+        result = {'type': 'relation', 'direction': 2, 'variable': None, 'flags': None, 'attributes': None}
+        if children:
+            for child in children[0]['value']:
+                result.update(child)
+        return {'value': result}
+
+    def visit_relation_any(self, node, children) -> object:
+        result = {'type': 'relation', 'direction': 0, 'variable': None, 'flags': None, 'attributes': None}
+        if children:
+            for child in children[0]['value']:
+                result.update(child)
+        return {'value': result}
+
+    def visit_relation_def(self, node, children) -> object:
+        return {'value': [child['value'] for child in children]}
+
+    def visit_node(self, node, children) -> object:
+        result = {'type': 'node', 'variable': None, 'flags': None, 'attributes': None}
+        for child in children:
+            result.update(child['value'])
+        return {'value': result}
+
+    def visit_assignment(self, node, children) -> object:
+        return {'value': {'variable': node.value}}
+
+    def visit_flags(self, node, children) -> object:
+        return {'value': {'flags': [child['value'] for child in children]}}
+
+    def visit_attributes(self, node, children) -> object:
+        return {'value': {'attributes': children[0]['value']}}
 
     def visit_part_return(self, node, children) -> object:
         return {'value': {'return': children[1]['value']}}
@@ -15,9 +85,9 @@ class GrammarVisitor(PTNodeVisitor):
         return {'value': [child for child in children]}
 
     def visit_selector(self, node, children) -> object:
-        value = children[0]['value']
-        value['as'] = children[1]['value']['as'] if len(children) > 1 else None
-        return value
+        result = children[0]['value']
+        result['as'] = children[1]['value']['as'] if len(children) > 1 else None
+        return result
 
     def visit_content(self, node, children) -> object:
         return {'value': children[0]['value']}
@@ -43,6 +113,9 @@ class GrammarVisitor(PTNodeVisitor):
 
     def visit_synonym(self, node, children) -> object:
         return {'value': {'as': children[1]['value']}}
+
+    def visit_flag(self, node, children) -> object:
+        return {'value': node.value[1:]}
 
     def visit_identifier(self, node, children) -> object:
         return {'value': node.value}
@@ -89,11 +162,20 @@ class GrammarVisitor(PTNodeVisitor):
     def visit_key_labels(self, node, children) -> object:
         return {"value": "LABELS"}
 
+    def visit_key_match(self, node, children) -> object:
+        return {"value": "MATCH"}
+
     def visit_key_null(self, node, children) -> object:
         return {"value": None}
 
     def visit_key_return(self, node, children) -> object:
         return {"value": "RETURN"}
+
+    def visit_key_rule(self, node, children) -> object:
+        return {"value": "RULE"}
+
+    def visit_key_salience(self, node, children) -> object:
+        return {"value": "SALIENCE"}
 
     def visit_key_true(self, node, children) -> object:
         return {"value": True}
@@ -106,10 +188,14 @@ class GrammarVisitor(PTNodeVisitor):
 
 
 if __name__ == '__main__':
-    content = 'RETURN {"some": "value"} AS dict, labels($v1), types($v2) AS types, id($v3), $v4 AS name, $v5.field'
+    content = 'RULE "This is an example" ' \
+              'SALIENCE 5 ' \
+              'MATCH ($v1 :label1 :labels2 {"alpha": "string", "beta": 123})-[$v2]->(:type1:type2) ' \
+              'MATCH ({"alpha": "string", "beta": 123})<--($v3:label3:label4)-[$v4{"current": True}]-(:label5{"main": True}) ' \
+              'RETURN {"some": "value"} AS dict, labels($v1), types($v2) AS types, id($v3), $v4 AS name, $v5.field; '
     print(content)
 
-    parser = ParserPython(part_return, comment)
+    parser = ParserPython(resource, comment)
     parse_tree = parser.parse(content)
     result = visit_parse_tree(parse_tree, GrammarVisitor())
     print(json.dumps(result['value'], indent=4))
